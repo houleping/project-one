e0931561b3d6208e03341c5725e596dc37328c5e
houleping <609025485@qq.com>
2020-12-18 18:57:17 +0800

    A

---
+++ src/main/java/com/example/projectone/abc/A.java
@@ -0,0 +1,13 @@
+package com.example.projectone.abc;
+
+/**
+ * @author hlp
+ * @version 1.0
+ * @Date 2020/12/18
+ */
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println("11111111111111111");
+    }
+}
